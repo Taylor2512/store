325fe3949c1c0a0cf9efcd222c1bb88979ce0c25
Jhonn Taylor Montenegro Arellano <desarrollo.16@torresytorres.com>
2025-01-13 21:34:46 -0500

    corregir estilo search

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/styles/**/*.{scss,css}" // ✔ Asegurar que se escanee SCSS
+    "./src/styles/**/*.{scss,css}"
   ],
   theme: {
     extend: {
